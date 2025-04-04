b893e99680c5f96f8b6ff4898e7cce37bdf4e10d
drex007 <vykeoj@gmail.com>
2025-04-04 14:33:57 +0100

    feat:rrd version updss

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,28 @@
-import { Switch, Route } from "wouter";
+// import { Switch, Route } from "wouter";
 import { queryClient } from "./lib/queryClient";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "@/components/ui/toaster";
 import NotFound from "@/pages/not-found";
 import Home from "@/pages/Home";
+import {Routes, Route, BrowserRouter} from "react-router-dom"
 
 function Router() {
   return (
-    <Switch>
-      <Route path="/" component={Home} />
-      <Route component={NotFound} />
-    </Switch>
+    <BrowserRouter>
+    
+    <Routes>
+      <Route path="/" element={< Home  />} />
+      <Route path="*" element={<NotFound />} />
+    </Routes>
+    
+    </BrowserRouter>
   );
 }
 
 function App() {
   return (
     <QueryClientProvider client={queryClient}>
-      <Router />
+     < Router  />
       <Toaster />
     </QueryClientProvider>
   );
